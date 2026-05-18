6c6a7ea5be5032f4a0a342d2abc12a12f315d855
kut86 <46892243+kut86@users.noreply.github.com>
2026-05-18 23:31:39 +0500

    firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,13 +1,11 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT"
+  apiKey: "AIzaSyCaduHQdeknkestReV2lgMsXyF_cluBHDE",
+  authDomain: "amigos-a993b.firebaseapp.com",
+  projectId: "amigos-a993b"
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const db = getFirestore(app);
