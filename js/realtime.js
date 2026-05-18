d4f0d184ccf4c0b3c1082298571efbcf3194b794
kut86 <46892243+kut86@users.noreply.github.com>
2026-05-18 23:33:28 +0500

    realtime.js

--- js/realtime.js
+++ js/realtime.js
@@ -1,8 +1,4 @@
-import {
-  collection,
-  onSnapshot
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-
+import { collection, onSnapshot } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { db } from "./firebase.js";
 
 export let weapons = {};
@@ -10,23 +6,15 @@ export let targets = {};
 
 export function startRealtime(callback) {
 
-  onSnapshot(collection(db, "weapons"), snap => {
+  onSnapshot(collection(db, "weapons"), (snap) => {
     weapons = {};
-
-    snap.forEach(doc => {
-      weapons[doc.id] = doc.data();
-    });
-
+    snap.forEach(doc => weapons[doc.id] = doc.data());
     callback();
   });
 
-  onSnapshot(collection(db, "targets"), snap => {
+  onSnapshot(collection(db, "targets"), (snap) => {
     targets = {};
-
-    snap.forEach(doc => {
-      targets[doc.id] = doc.data();
-    });
-
+    snap.forEach(doc => targets[doc.id] = doc.data());
     callback();
   });
 
