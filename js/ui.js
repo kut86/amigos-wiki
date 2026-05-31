51f0a3d3c9f39a13136f2792155214f11ca42105
kut86 <46892243+kut86@users.noreply.github.com>
2026-06-01 00:42:17 +0500

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -7,7 +7,7 @@ export function renderTable(results) {
       <tr>
         <th>Weapon</th>
         <th>Hits</th>
-        <th>Sulfur</th>
+        <th>Сера</th>
         <th>Time</th>
       </tr>
   `;
