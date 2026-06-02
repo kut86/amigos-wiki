b7e315fe16763b9ad67bd5a6a69b19a3fac6e87a
kut86 <46892243+kut86@users.noreply.github.com>
2026-06-02 20:17:26 +0500

    Update map.js

--- gallery/tarkov/map.js
+++ gallery/tarkov/map.js
@@ -1,1 +1,9 @@
-
+const firebaseConfig = {
+  apiKey: "...",
+  authDomain: "tarkovmap-376d0.firebaseapp.com",
+  projectId: "tarkovmap-376d0",
+  storageBucket: "tarkovmap-376d0.firebasestorage.app",
+  messagingSenderId: "...",
+  appId: "...",
+  databaseURL: "https://tarkovmap-376d0-default-rtdb.europe-west1.firebasedatabase.app"
+};
