f075e623fc273c03b5d6cb57a48ad8d4824bf915
kut86 <46892243+kut86@users.noreply.github.com>
2026-06-04 18:21:32 +0500

    Update app.js

--- galler/app.js
+++ galler/app.js
@@ -134,14 +134,14 @@ onAuthStateChanged(auth, user => {
 
 let pz = null;
 
-/*function initPanzoom() {
+function initPanzoom() {
   if (pz) { pz.destroy(); pz = null; }
 
   pz = Panzoom(mapEl, {
     maxScale: 8,
-    minScale: 0.2,
+    minScale: 0.1,
     contain:  "outside"
-  });*/
+  });
 
   mapEl.parentElement.addEventListener("wheel", pz.zoomWithWheel);
 
