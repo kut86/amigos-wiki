bc6f8a1708bd3db1d70bcdf795265f5837a81f06
kut86 <46892243+kut86@users.noreply.github.com>
2026-06-12 20:33:42 +0500

    Update app.js

--- map/app.js
+++ map/app.js
@@ -25,7 +25,7 @@ const provider = new GoogleAuthProvider();
 
 /* ── Карты ── */
 const MAPS = {
-  woods:       { label: "Лес",      imgUrl: "Woods2.avif",       fallback: "" },
+  woods:       { label: "Лес",      imgUrl: "Woods3.avif",       fallback: "" },
   customs:     { label: "Таможня",  imgUrl: "images/customs.jpg",     fallback: "https://i.imgur.com/FKY4S2W.jpg" },
   interchange: { label: "Развязка", imgUrl: "images/woods1.avif", fallback: "https://i.imgur.com/FKY4S2W.jpg" }
 };
