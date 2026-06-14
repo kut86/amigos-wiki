2baa4c84287d56b8e6098bd9f6d733156716c2f8
kut86 <46892243+kut86@users.noreply.github.com>
2026-06-14 10:31:53 +0500

    Update app.js

--- map/app.js
+++ map/app.js
@@ -29,7 +29,7 @@ const MAPS = {
   streetsoftarkov: { label: "Улицы Таркова", imgUrl: "images/streetsoftarkov/streetsoftarkov.jpg", fallback: "" },
   labs: { label: "Лаборатория", imgUrl: "images/labs/labs.jpg", fallback: "" },
   interchange: { label: "Развязка", imgUrl: "images/interchange/interchange.jpg", fallback: "" },
-  customs:     { label: "Таможня",  imgUrl: "images/customs/customs.jpg",  fallback: "" },
+  customs:     { label: "Таможня",  imgUrl: "images/customs/customs.jpg",  fallback: "https://github.com/kut86/amigos-wiki/blob/main/map/images/customs/customs.jpg?raw=true" },
   factory: { label: "Завод", imgUrl: "images/factory/factory.jpg", fallback: "" },
   woods:       { label: "Лес", imgUrl: "images/woods/woods.jpg", fallback: "" },
   reserve: { label: "Резерв", imgUrl: "images/reserve/reserve.jpg", fallback: "" },
